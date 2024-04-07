e8d3506e4cb1084b2116f26def90786a1a61a6aa
heepheep <qooroom@gmail.com>
2024-04-07 10:35:23 +0300

    Just try commit

--- server/main.go
+++ server/main.go
@@ -5,8 +5,13 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
+	router.GET("/file/:id", func(c *gin.Context) {
+
+	})
+
 	err := router.Run(":8081")
 	if err != nil {
 		return
 	}
+
 }
