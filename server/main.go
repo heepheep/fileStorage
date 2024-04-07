dbe5b2aaff99d4a62ec780c17acc70583d46186d
heepheep <qooroom@gmail.com>
2024-04-07 11:33:24 +0300

    Added api-routes

--- server/main.go
+++ server/main.go
@@ -5,10 +5,25 @@ import "github.com/gin-gonic/gin"
 func Main() {
 	router := gin.Default()
 
-	router.GET("/file/:id", func(c *gin.Context) {
-
-	})
-
+	api := router.Group("/api")
+	{
+		file := api.Group("/file")
+		{
+			file.GET("/all")    // in progress
+			file.GET("/:id")    // in progress
+			file.POST("/")      // in progress
+			file.PUT("/:id")    // in progress
+			file.DELETE("/:id") // in progress
+		}
+		auth := api.Group("/auth")
+		{
+			auth.POST("/registration") // in progress
+			auth.PUT("/change-pass")   // in progress
+			auth.PUT("/change-email")  // in progress
+			auth.POST("/login")        // in progress
+			auth.POST("/logout")       // in progress
+		}
+	}
 	err := router.Run(":8081")
 	if err != nil {
 		return
